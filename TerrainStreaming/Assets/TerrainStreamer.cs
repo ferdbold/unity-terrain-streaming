7528c6e5fb3f7398ea12198ed923adcccf32e5d8
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-01-26 21:22:35 -0500

    Initial project

---
+++ TerrainStreaming/Assets/TerrainStreamer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TerrainStreamer : MonoBehaviour {
+
+	[Tooltip("The terrain prefab to tile")]
+	[SerializeField]
+	private Terrain _terrain;
+
+	[Header("Streaming Properties")]
+	[SerializeField]
+	private float _outerconeLimit = 40f;
+
+	[SerializeField]
+	private float _innerRingLimit = 10f;
+
+	[SerializeField]
+	private float _coneRadius = 45f;
+	
+	private Camera _camera;
+	private List<List<Terrain>> _terrainMatrix = new List<List<Terrain>>();
+	
+	void Awake () {
+		_camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
